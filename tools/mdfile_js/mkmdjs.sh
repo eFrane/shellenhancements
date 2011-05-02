a728499a04ceece216ae995c574579ee2dc81448
Stefan Graupner <stefan.graupner@gmail.com>
2011-05-02 15:28:04 +0200

    some helper scripts for mdfile.

---
+++ tools/mdfile_js/mkmdjs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm -f md.js
+
+closure --warning_level QUIET --js jquery-1.5.2.js --js waypoints.js --js_output_file md.js
+
+has_pbcopy=`which -s pbcopy`
+if [ $? -eq 0 ];
+then
+    cat md.js | pbcopy
+    rm md.js
+    echo "Generated javascript is in pasteboard."
+else
+    echo "Generated javascript is in md.js"
+fi
+
