761f865af7743d92f375dcb4b0ae1fa71d9c5ab5
Stefan Graupner <stefan.graupner@gmail.com>
2011-05-03 22:36:28 +0200

    very simplified script version.

--- tools/mdfile_js/mkmdjs.sh
+++ tools/mdfile_js/mkmdjs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -f md.js
 
-closure --warning_level QUIET --js jquery-1.5.2.js --js jquery.metadata.js --js jquery.hoverIntent.min.js --js jquery.mb.flipText.js --js mbExtruder.js --js waypoints.js --js_output_file md.js
+closure --warning_level QUIET --js jquery-1.5.2.js --js_output_file md.js
 has_pbcopy=`which -s pbcopy`
 if [ $? -eq 0 ];
 then
