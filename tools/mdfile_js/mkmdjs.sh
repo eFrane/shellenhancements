ac0ad99684215c20ee28e6870a2db5f022c20b32
Stefan Graupner <stefan.graupner@gmail.com>
2011-05-03 23:01:16 +0200

    modified lib builder, added jquery scrollTo.

--- tools/mdfile_js/mkmdjs.sh
+++ tools/mdfile_js/mkmdjs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -f md.js
 
-closure --warning_level QUIET --js jquery-1.5.2.js --js_output_file md.js
+closure --warning_level QUIET --js jquery-1.5.2.js --js jquery.scrollTo.js --js_output_file md.js
 has_pbcopy=`which -s pbcopy`
 if [ $? -eq 0 ];
 then
